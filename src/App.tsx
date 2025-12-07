f2d40ae01482deabd37451c391410cc9235352a0
Felix Bredl <Elektro-Bredl@Outlook.de>
2025-12-07 09:12:13 +0100

    Build-Fehler behoben - auskommentierten Code entfernt

--- src/App.tsx
+++ src/App.tsx
@@ -3,33 +3,6 @@ import Maintenance from './pages/Maintenance';
 function App() {
   // Wartungsmodus aktiviert - Website ist offline
   return <Maintenance />;
-  
-  /* Original-Code für später (wenn Website wieder online):
-  import { BrowserRouter, Routes, Route } from 'react-router-dom';
-  import Navbar from './components/Navbar';
-  import OnePage from './pages/OnePage';
-  import Impressum from './pages/Impressum';
-  import Datenschutz from './pages/Datenschutz';
-  import Footer from './components/Footer';
-  import CookieBanner from './components/CookieBanner';
-
-  return (
-    <BrowserRouter>
-      <div className="min-h-screen bg-background flex flex-col">
-        <Navbar />
-        <main className="flex-grow">
-          <Routes>
-            <Route path="/" element={<OnePage />} />
-            <Route path="/impressum" element={<Impressum />} />
-            <Route path="/datenschutz" element={<Datenschutz />} />
-          </Routes>
-        </main>
-        <Footer />
-        <CookieBanner />
-      </div>
-    </BrowserRouter>
-  );
-  */
 }
 
 export default App;
